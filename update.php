5c5d4a4627eaf99793983c991d6fc320d5a16247
SaraiClaros <saraiclaros12@gmail.com>
2025-03-19 07:19:06 -0600

    Update update.php

--- update.php
+++ update.php
@@ -6,12 +6,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['accion']) && $_POST['
     $id = $_POST['id'];  
     $nombre = $_POST['nombre'];
     $edad = $_POST['edad'];
+    $estadia = $_POST['estadia'];
     $especie = $_POST['especie'];
     $color = $_POST['color'];
     $raza = $_POST['raza'];
 
     
-    $sql = "UPDATE mascotas_adp SET nombre='$nombre', edad='$edad', especie='$especie', color='$color', raza='$raza' WHERE id='$id'";
+    $sql = "UPDATE mascotas_adp SET nombre='$nombre', edad='$edad', estadia='$estadia', especie='$especie', color='$color', raza='$raza' WHERE id='$id'";
 
     if ($conn->query($sql) === TRUE) {
         $mensaje = "Mascota actualizada correctamente";
